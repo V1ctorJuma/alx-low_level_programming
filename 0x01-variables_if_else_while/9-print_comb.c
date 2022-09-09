b26ae5c714f4dae27bbad230e891319799dcf576
V1ctorJuma <jumavictor151@gmail.com>
2022-09-09 20:58:50 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+/*
+ * File 2-print_alphabet.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <=9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putchar (',');
+		putchar (' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
