42d367b781af4c9374fd04963f511b34d869f807
V1ctorJuma <jumavictor151@gmail.com>
2022-09-09 20:19:33 +0000

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+/*
+ * File 2-print_alphabet.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
