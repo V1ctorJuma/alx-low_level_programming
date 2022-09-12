032b73c37d53f343bc7e1603ea18af6c1b109919
V1ctorJuma <jumavictor151@gmail.com>
2022-09-12 13:40:58 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,37 +1,17 @@
-/*
- *
- *  * File: 5-printf.c
- *
- *   * 
- *
- *    */
-
-
+/* File: 5-printf.c */
 
 #include <stdio.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Prints "with proper grammar, but the outcome is
+ * Description - Prints with proper grammar, but the outcome is a piece of art
  *
- *                 a piece of art," followed by a new line.
- *
- *    *
- *
- *     * Return: Always 0.
- *
- *      */
-
+ * Return: Always 0 (success)
+ */
 int main(void)
-
 {
-
 		printf("with proper grammar, but the outcome is a piece of art,\n");
 
-
-
 			return (0);
-
 }
