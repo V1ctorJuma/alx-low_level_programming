ee5bdbfbea509c4820d6bee0026a69b6bf76911c
V1ctorJuma <jumavictor151@gmail.com>
2022-09-14 17:13:02 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,13 +11,14 @@ void print_alphabet_x10(void)
 	int count;
 	int letter;
 
-	for (count = 0; count <= 9; count++)
+	for (count = 0; count < 10; count++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
+
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
 
