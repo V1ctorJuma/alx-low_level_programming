d3cc4e67221aaeb27100e9fbd038a867b76c6a85
V1ctorJuma <jumavictor151@gmail.com>
2022-09-14 17:26:32 +0000

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - entry point
+ * @c: letter being tested
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	if ((c > 97 && c <= 122) || (c >= 97 && C <= 122))
+		return (1);
+	else
+		return (0);
+}
+
