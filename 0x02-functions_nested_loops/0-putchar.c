cdd973791ee775b4d3c4b166edd1dfbf1972c182
V1ctorJuma <jumavictor151@gmail.com>
2022-09-14 13:59:59 +0000

    nestedloops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+/*
+ * File: 0-putchar.c
+ */
+
+#include "main.h"
+/**
+ * main - entry point 
+ *
+ * description : prints _putchar, followed by a new line
+ *
+ * return : always return 0
+ */
+
+int main(void)
+{
+	_putchar('V');
+	_putchar('\n');
+
+	return (0);
+}
+
+
+
+
