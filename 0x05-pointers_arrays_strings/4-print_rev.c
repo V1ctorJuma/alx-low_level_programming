bd4158a3d3df1f892ff890690c77e858cde68bf2
V1ctorJuma <jumavictor151@gmail.com>
2022-09-20 02:32:36 +0000

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,13 +5,17 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, i;
+	int i, c = 0;
 
-	while (s[i++])
-		len++;
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		c++;
+	}
 
-	for (i = len - 1; i >= 0; i--)
-		_putchar(s[i]);
+	for (i = c - 1; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
 
 	_putchar('\n');
 }
