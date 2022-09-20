f3880f79b27d8524f91645240675194ca1ecd568
V1ctorJuma <jumavictor151@gmail.com>
2022-09-20 02:20:36 +0000

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
-	while (src[index])
+	for (; src[i] != '\0\; i++)
 	{
-		dest[index] = src[index];
-		index++;
+		dest[i] = src[i];
 	}
 
 	return (dest);
