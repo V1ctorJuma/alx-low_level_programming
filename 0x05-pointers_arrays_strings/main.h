4f6c774570dcb705ae9c71103666df9d7853d03b
V1ctorJuma <jumavictor151@gmail.com>
2022-09-20 01:39:31 +0000

    4-print_rev.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 
+void reverse_array(int *a, int n)
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 size_t _strlen(const char *str);
 void _puts(char *str);
-void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
