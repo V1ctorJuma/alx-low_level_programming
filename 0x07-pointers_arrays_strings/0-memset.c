ff2fc9eb93162b2c5547fe0619cfbaaaa118dc2b
V1ctorJuma <jumavictor151@gmail.com>
2022-09-26 20:05:03 +0000

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,18 @@
-#include "msin.h"
+#include "main.h"
 
 /**
- * _memset - fills memory with a constant byte .
- *@s: allocated memory - array of characters .
- * @b: char bytes .
- * @n: number of spaces to fill .
- * Return: pointer to memory area s
+ * _memset - Entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: Always 0 (Success)
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
 		s[i] = b;
-	}
+
 	return (s);
 }
