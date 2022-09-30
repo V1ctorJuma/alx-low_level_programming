820b2cccbb8e50313b56376a48f5c6d69751b242
V1ctorJuma <jumavictor151@gmail.com>
2022-09-30 19:42:51 +0000

    static Libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,3 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,5 +19,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-#endif /* HOLBERTON_H */
