233c6dda9a07cc5792b6d171ae28a523e6ec032a
V1ctorJuma <jumavictor151@gmail.com>
2022-09-30 16:19:31 +0000

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,12 +6,9 @@
  * @argv: array pointing to arguments
  * Return: 0
  */
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-		printf("%d\n", i - 1);
-
+	(void) argv;
+		printf("%d\n", argc - 1);
 	return (0);
 }
