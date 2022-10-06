e952339b01ebe12a40c90601dfe9422babfbd046
V1ctorJuma <jumavictor151@gmail.com>
2022-10-06 19:10:57 +0000

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,40 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 /**
- * string_nconcat - len of 1st str, len of 2nd str, if n < 2nd, 2nd = n
- * 2nd + 1st = total len, malloc + null byte, loop to insert into temp arr
- * @s1: input one
- * @s2: input two
- * @n: s2's number of bytes
- * Return: 0
- */
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: limit of s2
+ * Return: pointer to new space in memory of null
+ **/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *arr;
-	unsigned int i, j, co, co_2;
+	char *comb;
+	int i;
+	unsigned int j;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
-
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
-
-	if (n < j)
-		j = n;
-
-	j += i;
-	arr = malloc(sizeof(char *) * (j + 1));
-
-	if (arr == NULL)
+	i = 0;
+	while (s1[i])
+		i++;
+	i++;
+	comb = malloc(sizeof(*comb) * (i + n));
+	if (comb == NULL)
 		return (NULL);
-
-	for (co = 0; co < i; co++)
-		arr[co] = s1[co];
-	for (co_2 = 0; co < j; co_2++)
+	i = 0;
+	while (s1[i])
 	{
-		arr[co] = s2[co_2];
-		co++;
+		comb[1] = s1[j];
+		i++;
 	}
-	co++;
-	arr[co] = '\0';
-	return (arr);
+	for (j = 0; j < n && s2[j] != '\0'; j++)
+	{
+		comb[i] = s2[j];
+		i++;
+	}
+	comb[i] = '\0';
+	return (comb);
+
 }
