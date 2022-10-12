db89f26071aecf6e188674a4b54be6ad6522be7a
V1ctorJuma <jumavictor151@gmail.com>
2022-10-12 01:02:54 +0000

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,14 +1,17 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "dog.h"
+
 /**
- * free_dog - Entry Point
- * @d: struct to free
- * Return: 0
+ *free_dog - frees dogs
+ *@d: pointer to dog to free
+ *Return: void
  */
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
-	free(d);
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
